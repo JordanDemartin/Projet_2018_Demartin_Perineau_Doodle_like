9e2d67da90169da186e9a7db9853718eb9e98d34
perineau <flavien.perineau@etu.u-pec.fr>
2018-06-17 16:54:36 +0200

    modif minime css

--- application/views/doudle/creation.php
+++ application/views/doudle/creation.php
@@ -26,8 +26,7 @@ $( function() {
                 <label for="date_<?php echo $i ?>">Date :</label>
                 <input type="text" name="date_<?php echo $i ?>" pattern="(0[1-9]|[1-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/\d\d\d\d" required id="date_<?php echo $i ?>" class="calendrier" value=<?php echo set_value('date_'.$i);?>>
                 <label for="heure_<?php echo $i ?>">Heure :</label>
-                <input type="text" name="heure_<?php echo $i ?>" id="heure_<?php echo $i ?>" pattern="([0-1][0-9])|2[0-3]|([0-9])" required value=<?php echo set_value('heure_'.$i);?>>:
-                <input type="text" name="minute_<?php echo $i ?>" pattern="([0-5][0-9])|([0-9])" required value=<?php echo set_value('minute_'.$i);?>>
+                <input type="text" name="heure_<?php echo $i ?>" id="heure_<?php echo $i ?>" pattern="([0-1][0-9])|2[0-3]|([0-9])" required value=<?php echo set_value('heure_'.$i);?>> : <input type="text" name="minute_<?php echo $i ?>" pattern="([0-5][0-9])|([0-9])" required value=<?php echo set_value('minute_'.$i);?>>
             </div>
         <?php endfor; ?>
 
